7462d3b5e6fea903352a16d4bf237482b13f6413
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-09 02:42:02 -0300

    Validation: custom pipe to validate tasks status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatus = Object.values(TaskStatus);
+
+    transform(value: any){
+        
+        value = value.toUpperCase();
+
+        if(!this.isValid(value)){
+            throw new BadRequestException(`"${value}" is not a valid status`);
+        }
+        
+        return value;
+    }
+
+    private isValid(status: any): boolean {
+        const idx = this.allowedStatus.indexOf(status);
+
+        return idx !== -1;
+    }
+}
