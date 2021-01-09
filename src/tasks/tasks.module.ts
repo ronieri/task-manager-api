cfae140a94c9538c8a5648abad9f123fa194cf04
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-09 12:19:07 -0300

    Adding our task repository to the module via dependency injection

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaksRepository } from './task.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TaksRepository])
+  ],
   controllers: [TasksController],
   providers: [TasksService],
 })
