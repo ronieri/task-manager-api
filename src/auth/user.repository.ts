d038c1db98cb9e7716ccc9c9963dbb29e40b9f5d
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-09 16:35:03 -0300

    Security: Hashing passwords

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,15 +5,19 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.password = await this.hashPassword(password, salt);
+    
     try {
       await user.save();
     } catch (error) {
@@ -24,5 +28,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
