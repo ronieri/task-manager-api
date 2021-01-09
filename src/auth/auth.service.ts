25b025bc686d1ca70805563bf2af01a0965803e9
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-09 15:49:02 -0300

    Validation: auth credentials dto and password strength

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,5 +10,9 @@ export class AuthService {
     private userRepository: UserRepository,
   ) {
     //
+  }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
   }
 }
---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'password must contain at least one upper case and one lower case letter, and a number or a special character',
+  })
+  password: string;
+}
