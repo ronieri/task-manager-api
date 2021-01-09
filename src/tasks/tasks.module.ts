8dbe7dabc0fc9470275da878146ed62d45e7c6ed
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-09 19:20:05 -0300

    Guarding the tasks route

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaksRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaksRepository])],
+  imports: [TypeOrmModule.forFeature([TaksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
