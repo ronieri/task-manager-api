19181dc5880d9ebf7f9ba863cafd62a225f11434
Ronieri Sales <ronieri@ronieri.com.br>
2021-01-19 14:13:45 -0300

    Getting user task by id and updating task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -30,19 +30,19 @@ export class TasksController {
   @Get()
   getTasks(
     @Query(ValidationPipe) filterDto: GetTasksFilterDto,
-    @GetUser() user: User
+    @GetUser() user: User,
   ): Promise<Task[]> {
     return this.tasksService.getTasks(filterDto, user);
   }
 
   @Get('/:id')
-  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-    return this.tasksService.getTaskById(id);
+  getTaskById(@Param('id', ParseIntPipe) id: number, @GetUser() user: User,): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto, @GetUser() user: User): Promise<Task> {
+  createTask(@Body() createTaskDto: CreateTaskDto, @GetUser() user: User,): Promise<Task> {
     return this.tasksService.createTask(createTaskDto, user);
   }
 
@@ -55,7 +55,8 @@ export class TasksController {
   updateTaskStatus(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    @GetUser() user: User,
   ): Promise<Task> {
-    return this.tasksService.updateTaskStatus(id, status);
+    return this.tasksService.updateTaskStatus(id, status, user);
   }
 }
